a5b90a5a1545e561783481ee82ef41d03a90f260
abdoalshaibi <alshaibi3@gmail.com>
2024-04-05 01:13:53 +0300

    unit test CuisineControllerTest.java

--- src/test/java/itep/resturant/service/controller/CuisineControllerTest.java
+++ src/test/java/itep/resturant/service/controller/CuisineControllerTest.java
@@ -1,15 +1,12 @@
 package itep.resturant.service.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import itep.resturant.service.entity.Cuisine;
-import itep.resturant.service.repository.CuisineRepository;
 import itep.resturant.service.service.cuisine.CuisineServiceImpl;
 import itep.resturant.service.service.dto.CuisineRequestDto;
 import itep.resturant.service.service.dto.CuisineResponseDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,7 +14,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -29,12 +29,9 @@ class CuisineControllerTest {
     @MockBean
     CuisineServiceImpl service;
 
-    @Mock
-    CuisineRepository repository;
     @Autowired
     private MockMvc mockMvc;
     private CuisineResponseDto result;
-    private Cuisine cuisine;
     private CuisineRequestDto request;
     private static final ObjectMapper mapper = new ObjectMapper();
 
@@ -42,10 +39,7 @@ class CuisineControllerTest {
     @BeforeEach
     public void setup() {
 
-    cuisine  = Cuisine.builder()
-        .name("Japanese")
-        .description(null)
-        .build();
+
 
         request = CuisineRequestDto.builder()
                 .name("Japanese")
@@ -110,8 +104,21 @@ class CuisineControllerTest {
 
     @DisplayName("JUnit test for Get All Cuisine method")
     @Test
-    void testGetAll() {
+    void testGetAll() throws Exception {
 
+        CuisineResponseDto response = new CuisineResponseDto();
+        response.setName("Japanese");
+        response.setDescription(null);
 
+        when(service.GetAll()).thenReturn(List.of(response));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/cuisine")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .accept(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.*",hasSize(1)))
+                .andExpect(jsonPath("$..id").value(0))
+                .andExpect(jsonPath("$..name").value("Japanese"));
     }
 }
